026b8d77cf7d30941704444aa7add4c77b28c480
marcelnoehre <marcelnoehre@web.de>
2025-03-15 10:27:12 +0100

    basic dashbaord

---
+++ src/app/components/dashboard/dashboard.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { RouterModule } from '@angular/router';
+import { DashboardItem } from '../../interfaces/dashboard-item.interface';
+
+@Component({
+  selector: 'app-dashboard',
+  imports: [MatCardModule, RouterModule],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.scss'
+})
+export class DashboardComponent {
+  dashboardItems: DashboardItem[] = [];
+}
---
+++ src/app/interfaces/dashboard-item.interface.ts
@@ -0,0 +1,4 @@
+export interface DashboardItem {
+    title: string;
+    url: string;
+}
